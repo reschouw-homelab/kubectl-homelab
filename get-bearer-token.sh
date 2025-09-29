2d2f444333f1d7b188ff3b31ae320f70d8b89506
Ryan Schouweiler <reschouw@gmail.com>
2025-09-29 14:41:00 -0700

    quick script to remember how to get dashboard bearer token

--- get-bearer-token.sh
+++ get-bearer-token.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# get-bearer-token.sh
+# This script retrieves a bearer token for the admin-user in the kubernetes-dashboard namespace.
+
 kubectl -n kubernetes-dashboard create token admin-user
