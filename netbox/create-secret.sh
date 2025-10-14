beee269a7d108c4250a36d849f4bedc24bb5d7c7
Ryan Schouweiler <reschouw@gmail.com>
2025-10-14 12:27:15 -0700

    getting netbox container and postgres set up

--- netbox/create-secret.sh
+++ netbox/create-secret.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-# update-secret.sh
+# create-secret.sh
 
 # Creates the secret used for postgres credentials
 # Uses the default namespace, or whatever the current context is
 
+if kubectl describe secret netbox-postgres-secret >/dev/null 2>&1; then
+  echo "Secret already exists. Deleting current version..."
+  kubectl delete secret netbox-postgres-secret
+fi
+
 kubectl create secret generic netbox-postgres-secret \
   --from-literal=POSTGRES_USER=netbox \
   --from-literal=POSTGRES_PASSWORD="$(cat postgres-password.txt)" \
+  --from-literal=POSTGRES_SECRET_KEY="$(cat postgres-secret-key.txt)" 
   
